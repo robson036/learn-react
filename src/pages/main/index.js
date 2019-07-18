5f4c5ae6da16f93843d2c3d28192291cadb97e3f
robson036 <robson.miranda1991@gmail.com>
2019-07-17 23:05:30 -0300

    aula 8

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,58 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import "./styles.css";
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get("/products");
-    this.setState({ products: response.data.docs });
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const { docs, ...productInfo } = response.data;
+    this.setState({ products: docs, productInfo, page });
   };
+
+  prevPage = () => {
+    const page = this.state;
+    if (page === 1) return;
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  };
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+    if (page === productInfo.pages) return;
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
+  };
+
   render() {
+    const { products, page, productInfo } = this.state;
     return (
       <div className="product-list">
-        {this.state.products.map(product => (
-          <h2 key={product._id}>{product.title}</h2>
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a className="botao" href="">
+              Acessar
+            </a>
+          </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
+        </div>
       </div>
     );
   }
